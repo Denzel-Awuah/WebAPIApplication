aa67d6cbe7453c41f94347d0fc1d747888047cba
Denzel Awuah <denzelbawuah@gmail.com>
2024-11-29 14:26:56 -0500

    add authorization db

---
+++ WebApiApplication/Data/AuthorizationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiApplication.Data
+{
+    public class AuthorizationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthorizationDbContext(DbContextOptions<AuthorizationDbContext> options) : base(options)
+        {
+        }
+    }
+}
