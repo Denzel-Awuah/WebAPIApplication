f732e23ef4dc2fbfac71a3ddd06896a57739bbe9
Denzel Awuah <denzelbawuah@gmail.com>
2025-01-20 10:56:24 -0500

    testing courses

--- WebApiApplication.Tests/Unit Testing/TestCourseController.cs
+++ WebApiApplication.Tests/Unit Testing/TestCourseController.cs
@@ -37,7 +37,7 @@ namespace WebApiApplication.Unit_Testing
             var testCourse = new Course
             {
                 Id = 1,
-                Title = "Test Course Title",
+                Title = "A Title for the Test Course",
                 Description = "This is a test course"
             };
 
